20fa4dc291450464502aa9e7bffb708adf2d5118
Juan Esteban Berrío <juanestebanberrio@gmail.com>
2022-07-12 08:40:41 -0500

    commite_5
    commite_5

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable =
+    [
+        'name',
+        'reference',
+        'price',
+        'weight',
+        'stock',
+        'category_id'
+    ];
     use HasFactory;
 }
--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sale extends Model
 {
+
+    protected $fillable =
+    [
+        'quantity',
+        'product_id'
+    ];
+
     use HasFactory;
 }
